8ba3d685506c2557c339f19bfaeb5ee4fb9a5e58
mmuneeb10 <muhammadmuneeb10@gmail.com>
2025-02-23 07:30:15 +0500

    products api integrated with react query

--- src/App.js
+++ src/App.js
@@ -1,11 +1,39 @@
+import React from "react";
 import "./App.css";
 import { Routes } from "./routes";
 
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+const queryClient = new QueryClient({});
+
+const ReactQueryDevtoolsProduction = React.lazy(() =>
+  import("@tanstack/react-query-devtools/build/modern/production.js").then(
+    (d) => ({
+      default: d.ReactQueryDevtools,
+    })
+  )
+);
+
 function App() {
+  const [showDevtools, setShowDevtools] = React.useState(false);
+
+  React.useEffect(() => {
+    window.toggleDevtools = () => setShowDevtools((old) => !old);
+  }, []);
+
   return (
-    <div className="App">
-      <Routes />
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen />
+      {showDevtools && (
+        <React.Suspense fallback={null}>
+          <ReactQueryDevtoolsProduction />
+        </React.Suspense>
+      )}
+      <div className="App">
+        <Routes />
+      </div>
+    </QueryClientProvider>
   );
 }
 
--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,62 +1,31 @@
 import React from "react";
-import { Row, Col, Card, Button, Typography } from "antd";
+import { Row, Col, Card, Button, Typography, Spin } from "antd";
+import { useQuery } from "@tanstack/react-query";
+import { constants } from "../../constants";
 import "./home.css";
 
 const { Title, Text } = Typography;
 
-const products = [
-  {
-    id: 1,
-    name: "Maroon Waffle Knit POLO",
-    price: 10,
-    image:
-      "https://fittedshop.com/cdn/shop/files/1_42d3e4c2-a83c-4fdf-b5e7-df47509ef4b3.jpg?v=1739347987&width=1800",
-  },
-  {
-    id: 2,
-    name: "Oatmeal Basic POLO",
-    price: 11,
-    image:
-      "https://fittedshop.com/cdn/shop/files/3_c5d9e5d1-bf02-4448-a8b9-49a6f7079743.jpg?v=1738246923&width=1800",
-  },
-  {
-    id: 3,
-    name: "Mint Knitted POLO",
-    price: 12,
-    image:
-      "https://fittedshop.com/cdn/shop/files/1_28e45b30-250e-41bd-8908-d2bc7c6de46b.jpg?v=1738240468&width=1800",
-  },
-  {
-    id: 4,
-    name: "Black Knitted POLO",
-    price: 13,
-    image:
-      "https://fittedshop.com/cdn/shop/files/2_41316a32-49ee-4e06-b074-1db0cacc37dd.jpg?v=1716550491&width=1800",
-  },
-  {
-    id: 5,
-    name: "Lt Brown Knitted POLO",
-    price: 14,
-    image:
-      "https://fittedshop.com/cdn/shop/files/2_84142b58-c937-4f41-8469-eadacb829685.jpg?v=1738240568&width=1800",
-  },
-  {
-    id: 6,
-    name: "Green Forest Knit POLO",
-    price: 15,
-    image:
-      "https://fittedshop.com/cdn/shop/files/wokring.jpg?v=1721474120&width=1800",
-  },
-];
-
 const Home = () => {
+  const getProductsQuery = useQuery({
+    queryKey: ["products"],
+    queryFn: async () => {
+      const response = await fetch(`${constants.baseUrl}/products`);
+      return await response.json();
+    },
+  });
+
   return (
     <div className="home-container">
       <Title level={2} className="page-title">
         Products
       </Title>
-      <Row gutter={[16, 16,]}>
-        {products.map((product) => (
+      {!getProductsQuery.isLoading && getProductsQuery.data?.length === 0 && (
+        <p>There is no products to show</p>
+      )}
+
+      <Row gutter={[16, 16]}>
+        {getProductsQuery.data?.map((product) => (
           <Col key={product.id} xs={24} sm={12} md={8} lg={8}>
             <Card
               cover={<img alt={product.name} src={product.image} />}
